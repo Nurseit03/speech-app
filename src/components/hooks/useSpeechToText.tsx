89ec95c440d182841917553db10f925adcee7e71
Nurseit Bakytbekov <nurseitbakytbekov@gmail.com>
2024-03-18 13:23:34 +0600

    add useSpeechToText hooks, install mui icons

---
+++ src/components/hooks/useSpeechToText.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useRef, useState } from "react";
+
+interface SpeechRecognitionOptions {
+  interimResults?: boolean;
+  lang?: string;
+  continuous?: boolean;
+}
+
+const useSpeechToText = (options: SpeechRecognitionOptions = {}) => {
+  const [isListening, setIsListening] = useState(false);
+  const [transcript, setTranscript] = useState("");
+  const recgonitionRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (!("webkitSpeechRecognition" in window)) {
+      console.error("Web speeaach api is not supported");
+      return;
+    }
+
+    recgonitionRef.current = new  (window as any).webkitSpeechRecognition();
+    const recognition = recgonitionRef.current;
+    recgonitionRef.current.interimResults = options.interimResults || true;
+    recgonitionRef.current.lang = options.lang || "en-US";
+    recgonitionRef.current.continuous = options.continuous || false;
+
+    if ("webkitSpeechGrammarList" in window) {
+      const grammar =
+        "#JSGF V1.0; grammar punctuation; public <punc> = . | , | ? | ! | ; | : ;";
+      const speechRecognitionList = new  (window as any).webkitSpeechGrammarList();
+      speechRecognitionList.addFromString(grammar, 1);
+      recognition.grammars = speechRecognitionList;
+    }
+
+    recognition.onresult = (event: any) => {
+      let text = "";
+      for (let i = 0; i < event.results.length; i++) {
+        text += event.results[i][0].transcript;
+      }
+      setTranscript(text);
+    };
+
+    recognition.onerror = (event: any) => {
+      console.error("Speech recognition error: ", event.error);
+    };
+
+    recognition.onend = () => {
+      setIsListening(false);
+      setTranscript("");
+    };
+
+    return () => {
+      recognition.stop();
+    };
+  }, []);
+
+  const startListening = () => {
+    if (recgonitionRef.current && !isListening) {
+      recgonitionRef.current.start();
+      setIsListening(true);
+    }
+  };
+
+  const stopListening = () => {
+    if (recgonitionRef.current && isListening) {
+      recgonitionRef.current.stop();
+      setIsListening(false);
+    }
+  };
+
+  return {
+    isListening,
+    transcript,
+    startListening,
+    stopListening,
+  };
+};
+
+export default useSpeechToText;
--- src/components/voice-input/index.tsx
+++ src/components/voice-input/index.tsx
@@ -1,9 +1,27 @@
 import { Box } from "@mui/material";
 import { useState } from "react";
 import Button from "../ui/Button";
+import useSpeechToText from "../hooks/useSpeechToText";
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
 
 export const VoiceInput = () => {
   const [textInput, setTextInput] = useState("");
+
+  const { isListening, transcript, startListening, stopListening } =
+    useSpeechToText({ continuous: true });
+
+  const startStopListening = () => {
+    isListening ? stopVoiceInput() : startListening();
+  };
+
+  const stopVoiceInput = () => {
+    setTextInput(
+      (prevVal) =>
+        prevVal +
+        (transcript.length ? (prevVal.length ? "" : "") + transcript : "")
+    );
+    stopListening();
+  };
 
   return (
     <Box
@@ -16,17 +34,22 @@ export const VoiceInput = () => {
       }}
     >
       <Button
+        onClick={() => {
+          startStopListening();
+        }}
+        endIcon={<KeyboardVoiceIcon />}
         sx={{
-          backgroundColor: "#008744",
+          backgroundColor: isListening ? "#d62d20" : "#008744",
           color: "white",
           padding: "10px 20px",
           border: "none",
           borderRadius: "5px",
           cursor: "pointer",
           transition: "background-color 0.3 ease",
+          width: "min-content"
         }}
       >
-        Button
+        {isListening ? "Stop listening" : "Speak"}
       </Button>
       <textarea
         style={{
@@ -41,7 +64,15 @@ export const VoiceInput = () => {
           backgroundColor: "#f8f8f8",
           color: "#333",
         }}
-        value={textInput}
+        disabled={isListening}
+        value={
+          isListening
+            ? textInput +
+              (transcript.length
+                ? (textInput.length ? "" : "") + transcript
+                : "")
+            : textInput
+        }
         onChange={(e: any) => {
           setTextInput(e.target.value);
         }}
