6ba66f5fd935de03cd41d8cd22343600a895992f
Nurseit Bakytbekov <nurseitbakytbekov@gmail.com>
2024-04-11 10:04:27 +0600

    add copy tooltip

--- src/components/speech-recognizer/SpeechRecognizer.tsx
+++ src/components/speech-recognizer/SpeechRecognizer.tsx
@@ -1,27 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, IconButton, Tooltip } from "@mui/material";
 import VoiceInput from "../voice-input/VoiceInput";
 import ShareIcon from "@mui/icons-material/Share";
 import ArchiveIcon from "@mui/icons-material/Archive";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 
 export default function SpeechRecognizerContent() {
+  const [text, setText] = useState<string>("");
+
   const handleGetText = (text: string) => {
-    console.log(text);
+    setText(text);
   };
 
   return (
-    <Box sx={{ display: "flex", flexDirection: "row", gap: "10px", justifyContent: "center", alignItems: "center" }}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
       <VoiceInput getText={handleGetText} />
-      <Tooltip title="Поделиться" arrow>
-        <IconButton onClick={() => console.log("Поделиться")}>
-          <ShareIcon />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="В архив" arrow>
-        <IconButton onClick={() => console.log("архив")}>
-          <ArchiveIcon />
-        </IconButton>
-      </Tooltip>
+      <Box
+        sx={{
+          display: text ? "flex" : "none",
+          flexDirection: "row",
+          gap: "10px",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Tooltip title="В архив" arrow>
+          <IconButton onClick={() => console.log("архив")}>
+            <ArchiveIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Скопировать" arrow>
+          <IconButton onClick={() => console.log("копир")}>
+            <ContentCopyIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Поделиться" arrow>
+          <IconButton onClick={() => console.log("Поделиться")}>
+            <ShareIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </Box>
   );
 }
